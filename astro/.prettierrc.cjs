fa649ef2c71bdfe1157fee3d901f67d74e086cdb
Łukasz Płonka <lukasz@flyhigh.pro>
2024-02-14 14:26:49 +0100

    Configure project

---
+++ astro/.prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  singleQuote: true,
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
