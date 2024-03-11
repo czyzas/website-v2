946ad80c468716edd440b72fb4765a674b3e5276
Jakub Bazgier <kuba@flyhigh.pro>
2024-03-11 14:33:33 +0100

    feat: add node adapter

--- sweep-astro/astro.config.ts
+++ sweep-astro/astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defaultLocale, locales } from './src/i18n/config';
@@ -9,7 +10,7 @@ const appMode = process.env.APP_MODE;
 // https://astro.build/config
 export default defineConfig({
   output: appMode === 'ssr' ? 'server' : 'static',
-  adapter: appMode === 'ssr' ? vercel() : undefined,
+  adapter: appMode === 'ssr' ? node({ mode: 'standalone' }) : undefined,
   i18n: {
     defaultLocale,
     locales: [...locales],
