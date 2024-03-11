6d22b9ee21e3aa9093312955948586f8863dcb81
Lukasz Plonka <lukasz@flyhigh.pro>
2024-03-11 15:45:14 +0100

    Change index location in pages tree

--- sweep-astro/astro.config.ts
+++ sweep-astro/astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-// import vercel from '@astrojs/vercel/serverless';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
+// import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defaultLocale, locales } from './src/i18n/config';
@@ -10,7 +10,7 @@ const appMode = process.env.APP_MODE;
 // https://astro.build/config
 export default defineConfig({
   output: appMode === 'ssr' ? 'server' : 'static',
-  adapter: appMode === 'ssr' ? node({ mode: 'standalone' }) : undefined,
+  adapter: appMode === 'ssr' ? vercel() : undefined,
   i18n: {
     defaultLocale,
     locales: [...locales],
