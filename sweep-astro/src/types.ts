d4ee5e9b65e7083a5b0d5b0e1a14bed2b368d564
Jakub Bazgier <kuba@flyhigh.pro>
2024-03-20 12:28:27 +0100

    refactor: svg in images, mobile fixes

--- sweep-astro/src/types.ts
+++ sweep-astro/src/types.ts
@@ -3,3 +3,5 @@ import type { HTMLAttributes } from 'astro/types';
 export type DynamicTagAttributes<
   T extends keyof astroHTML.JSX.DefinedIntrinsicElements,
 > = Partial<astroHTML.JSX.IntrinsicAttributes & HTMLAttributes<T>>;
+
+export type WithAttributes<T> = astroHTML.JSX.IntrinsicAttributes & T;
