dda36036db2cdbb0e04f74c0c8f6ebb61a044a49
Jakub Bazgier <kuba@flyhigh.pro>
2024-05-07 15:53:34 +0200

    feat(wp): implement sorting by meta field and filtering by tax slug (wip)

--- sweep-wordpress/includes/functions/graphql.php
+++ sweep-wordpress/includes/functions/graphql.php
@@ -104,3 +104,97 @@ add_action( 'graphql_register_types', function () {
 	} catch ( Exception $e ) {
 	}
 } );
+
+// TODO: FILTER BY CATEGORY SLUG
+// https://stackoverflow.com/a/76356744
+
+//// First, we register the field in the "where" clause.
+//add_action( 'graphql_register_types', function () {
+//	try {
+//		$customposttype_graphql_single_name = "Country"; // Replace this with your custom post type single name in PascalCase
+//
+//		// Registering the 'categorySlug' argument in the 'where' clause.
+//		// Feel free to change the name 'categorySlug' to something that suits your requirements.
+//		register_graphql_field( 'RootQueryTo' . $customposttype_graphql_single_name . 'ConnectionWhereArgs', 'categorySlug', [
+//			'type'        => [ 'list_of' => 'String' ], // To accept multiple strings
+//			'description' => __( 'Filter by post objects that have the specific category slug', 'your_text_domain' ),
+//		] );
+//	} catch ( Exception $e ) {
+//	}
+//} );
+//
+//// Next, we add a filter to modify the query arguments.
+//add_filter( 'graphql_post_object_connection_query_args', function (
+//	$query_args,
+//	$source,
+//	$args,
+//	$context,
+//	$info
+//) {
+//
+//	$categorySlug = $args['where']['categorySlug']; // Accessing the 'categorySlug' argument.
+//
+//	if ( isset( $categorySlug ) ) {
+//		// If the 'categorySlug' argument is provided, we add it to the tax_query.
+//		// For more details, refer to the WP_Query class documentation at https://developer.wordpress.org/reference/classes/wp_query/
+//		$query_args['tax_query'] = [
+//			[
+//				'taxonomy' => 'your_taxonomy',
+//				// Replace 'your_taxonomy' with your actual taxonomy key
+//				'field'    => 'slug',
+//				'terms'    => $categorySlug
+//			]
+//		];
+//	}
+//
+//	return $query_args;
+//}, 10, 5 );
+
+// TODO: SORT BY META KEY
+// https://github.com/wp-graphql/wp-graphql/issues/287#issuecomment-341930784
+add_filter( 'graphql_queryArgs_fields', function ( $fields ) {
+	$fields['metaKey'] = [
+		'type'        => "String",
+		'description' => __( 'Show posts with a specific meta_key.', 'your-custom-plugin' ),
+	];
+
+	return $fields;
+} );
+
+add_filter( 'graphql_orderby_values', function ( $values ) {
+	$values['META_VALUE'] = [
+		'value'       => 'meta_value',
+		'description' => __( 'Order by meta_value', 'your-custom-plugin' ),
+	];
+	$values['META_VALUE_NUM'] = [
+		'value'       => 'meta_value_num',
+		'description' => __( 'Order by meta_value_num', 'your-custom-plugin' ),
+	];
+
+	return $values;
+} );
+
+add_filter( 'graphql_post_object_connection_query_args', function (
+	$query_args,
+	$source,
+	$args,
+	$context,
+	$info
+) {
+	if ( !empty( $args['where']['metaKey'] ) ) {
+		$query_args['meta_key'] = esc_html( $args['where']['metaKey'] );
+	}
+
+	return $query_args;
+}, 10, 5 );
+
+//add_filter( 'graphql_post_fields', function( $fields ) {
+//	$fields['someNumber'] = [
+//		'type' => "Int",
+//		'resolve' => function( $post ) {
+//			$value = get_post_meta( $post->ID, 'some_number', true );
+//			return absint( $value );
+//		}
+//	];
+//	return $fields;
+//} );
