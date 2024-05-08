6cce9a30396f0b1c81cc997e2641dc7bea4268f8
Jakub Bazgier <kuba@flyhigh.pro>
2024-05-08 13:58:18 +0200

    chore: remove unused store data

--- sweep-astro/src/stores/cmsStore.ts
+++ sweep-astro/src/stores/cmsStore.ts
@@ -1,28 +1,16 @@
 import type {
-  ContactPageQuery,
-  DemoPageQuery,
   HomepageQuery,
-  LanguagesFragment,
   MenuFragment,
-  Page,
   ThemeOptionsFragment,
 } from '@/__generated__/cms';
 import { createStore } from '@/scripts/store';
 
 export const cmsStore = createStore<{
-  languages?: (LanguagesFragment | undefined)[];
   homepage?: HomepageQuery['page'];
-  defaultPage?: Page;
-  contactPage?: ContactPageQuery;
-  demoPage?: DemoPageQuery;
   primaryMenu?: MenuFragment;
   themeOptions?: ThemeOptionsFragment;
 }>({
-  languages: [],
   homepage: undefined,
-  defaultPage: undefined,
-  contactPage: undefined,
-  demoPage: undefined,
   primaryMenu: undefined,
   themeOptions: undefined,
 });
