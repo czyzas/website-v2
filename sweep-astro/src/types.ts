ce665981a7bf1deb65923a8a81fa67ad2e502993
Jakub Bazgier <kuba@flyhigh.pro>
2024-05-09 16:26:31 +0200

    feat: add top columns distribution

--- sweep-astro/src/types.ts
+++ sweep-astro/src/types.ts
@@ -6,3 +6,5 @@ export type ReplaceTypenameLiteral<ObjType extends object> = {
     ? ReplaceTypenameLiteral<ObjType[KeyType]>
     : StringLiteralToString<ObjType[KeyType]>;
 };
+
+export type ColumnSizes = '5-7' | '6-6' | '7-5';
