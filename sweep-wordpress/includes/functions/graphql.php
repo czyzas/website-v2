db2b45b0650bee013e1d4e85cf7c5873714ccdff
Jakub Bazgier <kuba@flyhigh.pro>
2024-05-28 13:29:45 +0000

    feat(wp): wordpress pagination extension

--- sweep-wordpress/includes/functions/graphql.php
+++ sweep-wordpress/includes/functions/graphql.php
@@ -5,3 +5,4 @@ include_once 'graphql/modules-layout-name.php';
 include_once 'graphql/query-by-lang.php';
 include_once 'graphql/filter-by-category-slug.php';
 include_once 'graphql/sort-by-meta-key.php';
+include_once 'graphql/pagination.php';
