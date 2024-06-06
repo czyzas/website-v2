dba0bf2d20d4c3b83acb9bdbfb69eae075baf485
Jakub Bazgier <kuba@flyhigh.pro>
2024-06-06 10:13:56 +0200

    refactor: fetch insights along with homepage

--- sweep-astro/src/stores/cmsStore.ts
+++ sweep-astro/src/stores/cmsStore.ts
@@ -8,11 +8,13 @@ import { createStore } from '@/scripts/store';
 
 export const cmsStore = createStore<{
   homepage?: HomepageQuery['page'];
+  homepageInsights?: HomepageQuery['insights'];
   primaryMenu?: MenuFragment;
   subpageSettings?: SubpageSettingsFragment;
   themeOptions?: ThemeOptionsFragment;
 }>({
   homepage: undefined,
+  homepageInsights: undefined,
   primaryMenu: undefined,
   subpageSettings: undefined,
   themeOptions: undefined,
