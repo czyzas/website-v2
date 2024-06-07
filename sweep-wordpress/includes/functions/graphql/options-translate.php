8688fb3e124425f023a61ba3fa4c2bafcd8ef438
Jakub Bazgier <kuba@flyhigh.pro>
2024-06-07 14:47:00 +0200

    feat(wp): translate acf options

--- sweep-wordpress/includes/functions/graphql/options-translate.php
+++ sweep-wordpress/includes/functions/graphql/options-translate.php
@@ -5,29 +5,41 @@ use WPGraphQL\Utils\Utils;
 /**
  * Registers a new "wpmlLanguage" input field on ACF options pages queries, and
  * uses translated options page content based on the language code supplied
- * @throws Exception
+ * @copyright  [SOURCE](https://github.com/Ririshi/wp-graphql-wpml/blob/feat/acf-options-pages-language-filter/acf-compatibility.php)
  */
-// SOURCE: https://github.com/Ririshi/wp-graphql-wpml/blob/feat/acf-options-pages-language-filter/acf-compatibility.php
 add_action( 'graphql_register_types', function () {
-	register_graphql_field( 'RootQuery', 'themeOptionsByLang', [
-		'type'    => 'ThemeOptions',
-		'args'    => [
-			'lang' => [
-				'type'        => 'String',
-				'description' => 'Filter by WPML language code',
-			],
-		],
-		'resolve' => function ( $unused, $args ) {
-			if ( isset( $args['lang'] ) ) {
-				$lang = $args['lang'];
-				do_action( 'wpml_switch_language', $lang );
-				acf_update_setting( 'current_language', $lang );
+	try {
+		$options_pages = acf_get_options_pages();
+		foreach ( $options_pages as $options_page ) {
+			// Check if the option page should be shown in GraphQL schema
+			if ( !isset( $options_page['show_in_graphql'] ) || false === (bool)$options_page['show_in_graphql'] ) {
+				continue;
 			}
-			$options_page = acf_get_options_pages()['options'];
+			$type_name = $options_page['graphql_type_name'];
+			$field_name = graphql_format_field_name( $type_name . 'ByLang' );
 
-			return $options_page;
+			register_graphql_field( 'RootQuery', $field_name, [
+				'type'    => $type_name,
+				'args'    => [
+					'lang' => [
+						'type'        => 'String',
+						'description' => 'Filter by WPML language code',
+					],
+				],
+				'resolve' => function ( $unused, $args ) use ( $options_page ) {
+					if ( isset( $args['lang'] ) ) {
+						$lang = $args['lang'];
+						do_action( 'wpml_switch_language', $lang );
+						acf_update_setting( 'current_language', $lang );
+					}
+
+					return !empty( $options_page ) ? $options_page : null;
+				}
+			] );
 		}
-	] );
+	} catch ( Exception $exception ) {
+		trigger_error( $exception->getMessage(), E_USER_WARNING );
+	}
 } );
 
 //		$options_pages = acf_get_options_pages();
