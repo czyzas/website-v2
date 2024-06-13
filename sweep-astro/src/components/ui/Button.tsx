d4b7f9409bf8810b4ec42f2e21e338d87e3d62a7
Jakub Bazgier <kuba@flyhigh.pro>
2024-06-13 15:12:24 +0200

    feat: salesloft form on demo page

--- sweep-astro/src/components/ui/Button.tsx
+++ sweep-astro/src/components/ui/Button.tsx
@@ -2,15 +2,33 @@ import type { VariantProps } from 'class-variance-authority';
 import type { ComponentPropsWithoutRef } from 'react';
 import { cn } from '@/scripts/cn';
 import { buttonVariants } from '../../styles/buttonVariants';
+import Arrow from '@/assets/icons/arrow-right.svg?react';
+import { Spinner } from '../Spinner';
 
 interface ButtonProps
   extends ComponentPropsWithoutRef<'button'>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  arrow?: boolean;
+  loading?: boolean;
+}
 
 export const Button = ({
   variant = 'primary',
+  arrow = false,
+  loading = false,
   className,
+  children,
   ...rest
 }: ButtonProps) => (
-  <button {...rest} className={cn(buttonVariants({ variant }), className)} />
+  <button
+    {...rest}
+    className={cn(
+      buttonVariants({ variant }),
+      { 'pointer-events-none': loading },
+      className
+    )}
+  >
+    {!loading ? children : <Spinner className={cn('h-5 w-5 animate-spin')} />}
+    {!loading && arrow ? <Arrow className="size-[1.125em]" /> : null}
+  </button>
 );
